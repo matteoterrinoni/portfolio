a5a145ea96210d7bf07155102500b42ff12f1f5c
matteoterrinoni <matteo.terrinoni@gmail.com>
2017-10-05 03:46:40 +0200

    Delete firebaseConfig.js

--- app/containers/App/firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-export default {
-	apiKey: "AIzaSyDAcZBGfq6aAZl8B-oeqX8jIxryJAEQg3A",
-	authDomain: "portfolio-d6c32.firebaseapp.com",
-	databaseURL: "https://portfolio-d6c32.firebaseio.com",
-	projectId: "portfolio-d6c32",
-	storageBucket: "gs://portfolio-d6c32.appspot.com/",
-	messagingSenderId: "763692698732"
-};
